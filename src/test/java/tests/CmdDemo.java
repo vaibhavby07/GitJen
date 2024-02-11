14b906d8f404ce5963248e57e1ab6caff01079ae
vaibhavby07 <vaibhavby07@gmail.com>
2024-02-11 14:21:56 +0530

    send from cmd

---
+++ src/test/java/tests/CmdDemo.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class CmdDemo {
+
+	public static void main(String[] args) 
+	{
+	 System.out.println("Start cmd practice");
+	 System.out.println("Open github Repositories from cmd");
+	}
+}
